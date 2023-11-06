e7123fe5ad515f22c7dae9723f64818ef364b7a9
JadhavVaibhav3335 <114633277+JadhavVaibhav3335@users.noreply.github.com>
2023-11-06 10:27:03 +0530

    Set_03.scala

--- Set_03.scala
+++ Set_03.scala
@@ -1,23 +1,22 @@
-object MergeSetsProductAverage {
-  def main(args: Array[String]): Unit = {
-    println("Enter elements for the first set (comma-separated):")
-    val inputSet1 = scala.io.StdIn.readLine().split(",").map(_.trim).map(_.toInt).toSet
-
-    println("Enter elements for the second set (comma-separated):")
-    val inputSet2 = scala.io.StdIn.readLine().split(",").map(_.trim).map(_.toInt).toSet
-
-    val mergedSet = inputSet1.union(inputSet2)
-
-    if (mergedSet.nonEmpty) {
-      val product = mergedSet.product
-      val average = mergedSet.sum.toDouble / mergedSet.size
-
-      println("Merged Set:")
-      mergedSet.foreach(println)
-      println(s"Product of all elements: $product")
-      println(s"Average of all elements: $average")
-    } else {
-      println("The merged set is empty. Please enter some elements.")
-    }
-  }
+object merge
+{
+	def main(args: Array[String])
+	{
+		val n1 = Set(10, 20 ,45)
+		val n2 = Set(45, 30, 90)
+		
+		println("Set 1: " + n1)
+		println("Set 2: " + n2)
+		
+		val n = n1 ++ n2
+		println("Merged sets: " +n)
+		
+		val pr = n.product
+		println("Product is: " + pr)
+		
+		var len = n.size
+		val s = n.sum
+		val avg = s/len
+		println("Average is: " + avg)
+	}
 }
