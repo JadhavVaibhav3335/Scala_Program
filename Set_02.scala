eaa8ec68d303e159fa673f936a148502168d1c9a
JadhavVaibhav3335 <114633277+JadhavVaibhav3335@users.noreply.github.com>
2023-11-06 10:26:29 +0530

    Set_02.scala

--- Set_02.scala
+++ Set_02.scala
@@ -1,18 +1,12 @@
-object LargestAndSmallestElement {
-  def main(args: Array[String]): Unit = {
-    println("Enter elements for the set (comma-separated):")
-    val inputSet = scala.io.StdIn.readLine().split(",").map(_.trim).toSet
-
-    if (inputSet.nonEmpty) {
-      val intSet = inputSet.map(_.toInt)
-
-      val smallest = intSet.min
-      val largest = intSet.max
-
-      println(s"Smallest element in the set: $smallest")
-      println(s"Largest element in the set: $largest")
-    } else {
-      println("The set is empty. Please enter some elements.")
-    }
-  }
+object minmax
+{
+	def main(args: Array[String])
+	{
+		val n = List(1, 2, 4, 5, 6, 7, 8, 9, 10)
+		
+		print("List: " + n)
+		print("\nSmallest number: " + n.min)
+		print("\nLargest number: " + n.max)
+		println()
+	}
 }
